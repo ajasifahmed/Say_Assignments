d61d29510dd39654a85085730cd5ac99d7b7681d
asifdecem2 <asifdecem2@gmail.com>
2020-06-25 20:46:53 +0500

    Calcultor js

---
+++ Calculator-JS/app.js
@@ -0,0 +1,26 @@
+function clearText()
+    {
+        var result=document.getElementById('result');
+        result.value='';
+    }
+function cE()
+{
+    var result=document.getElementById('result');
+    var result=result.value;
+    var result=result.clear();
+}
+function getNumber(num)
+{
+    var txt=document.getElementById('result');
+    txt.value+=num;
+}
+function getNumber(num)
+{
+    var txt=document.getElementById('result');
+    txt.value+=num;
+}
+function getResult()
+{
+    var result=document.getElementById('result');
+    result.value=eval(result.value);
+}
